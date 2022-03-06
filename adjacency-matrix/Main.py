d1da33c80ff2b81e5084284d297c8c34d90b7e37
tudemaha <gede_mahardika88@yahoo.co.id>
2022-03-06 21:18:13 +0800

    connection checker for adjacency matrix

--- adjacency-matrix/Main.py
+++ adjacency-matrix/Main.py
@@ -1,17 +1,25 @@
+# import library
 import numpy as np
 
+# program title
 print('===== PROGRAM MATRIKS KETETANGGAAN =====')
 
+# variable declaration and initialization
 matrix = []
 nodes = int(input('\nBanyak node: '))
 
+# show instructions
 print('\nMasukkan 1 jika node terhubung, 0 jika tidak terhubung')
 
+# insert adjacency matrix elements
 for row in range(nodes):
     matrix.append([])
     for column in range(nodes):
         matrix[row].append(int(input(f'Baris {row + 1}, Kolom {column + 1}: ')))
 
+# make matrix using numpy
 matrix = np.array(matrix)
+
+# show adjacency matrix
 print('\nMatriks ketetanggaan:')
 print(matrix)
---
+++ connected-graph/Main.py
@@ -0,0 +1,51 @@
+# import library
+import numpy as np
+
+# program title
+print('===== PROGRAM KETERHUBUNGAN GRAPH =====')
+
+# variables declaration and initialization
+nodes = int(input('\nBanyak node: '))
+matrix = []
+
+# insert adjacency matrix elements
+print('\nMasukkan 1 jika node terhubung, 0 jika tidak terhubung')
+for row in range(nodes):
+    matrix.append([])
+    for column in range(nodes):
+        matrix[row].append(int(input(f'Baris {row + 1}, Kolom {column + 1}: ')))
+
+# make adjacency matrix using numpy
+matrix = np.array(matrix)
+# make helper matrix by slicing the adjacency matrix at row 1 from column 2 to end 
+helper_matrix = matrix[0, 1:]
+
+# show first state of matrices
+print('\nMatriks ketetanggaan:')
+print(matrix)
+print('\nMatriks bantuan awal:')
+print(helper_matrix)
+
+# check for the connection of graph in adjacency matrix
+for row in range(nodes - 1):
+    if helper_matrix[row] == 1:
+        for column in range(nodes - 1):
+            if (matrix[row][column] == 1) and (helper_matrix[column] == 0):
+                helper_matrix[column] = 1
+
+# show last state of matrices
+print('\nMatriks bantuan akhir:')
+print(helper_matrix)
+
+# check the connection status of graph
+status = True
+for helper in helper_matrix:
+    if helper == 0:
+        status = False
+        break
+
+# print the connection status
+if status == True:
+    print("Graph terhubung!")
+else:
+    print("Graph tidak terhubung!")
