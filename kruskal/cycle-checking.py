10e6aee8162d5ddea75d74f71299141ffa1495e3
tudemaha <gede_mahardika88@yahoo.co.id>
2022-04-15 00:07:59 +0800

    processing graph cycle algorithm

--- kruskal/cycle-checking.py
+++ kruskal/cycle-checking.py
@@ -4,8 +4,8 @@ matrix = [
     [0, 1, 1, 0, 0],
     [1, 0, 0, 1, 1],
     [1, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0],
-    [0, 1, 0, 0, 0]
+    [0, 1, 0, 0, 1],
+    [0, 1, 0, 1, 0]
 ]
 matrix = np.array(matrix)
 
@@ -22,19 +22,16 @@ while edge < vertices - 1:
             queue.append(i)
             flag[i] += 1
 
-        if (i in queue) and (flag[i] == 0):
-            visited.append(queue.pop(queue.index(i)))
-            flag[i] += 1
-            status = 1            
-
         for j in range(vertices):
             if (matrix[i][j] == 1) and (flag[j] == -1):
+                status = 1
                 queue.append(j)
                 flag[j] += 1
                 matrix[i][j] = matrix[j][i] = 0
 
-        if status == 1:
-            status = 0
+        if (i in queue) and (flag[i] == 0) and (status == 1):
+            visited.append(queue.pop(queue.index(i)))
+            flag[i] += 1
             edge += 1
             break
 
@@ -42,3 +39,14 @@ print(flag)
 print(queue)
 print(visited)
 print(matrix)
+
+for i in range(vertices):
+    for j in range(vertices):
+        if matrix[i][j] == 1:
+            status = 2
+            break
+    if status == 2:
+        break
+
+if status == 2:
+    print("Graph mengandung cycle!")
