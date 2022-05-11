d6e1b1ada80eb5cc342d2e76f536d4c30436633b
tudemaha <gede_mahardika88@yahoo.co.id>
2022-05-11 14:25:44 +0800

    finishing kruskal's algorithm in c++

--- minimum_spanning_tree/graph.h
+++ minimum_spanning_tree/graph.h
@@ -1,8 +1,62 @@
-void cycle_detection(int vertices, std::array<std::array<int, 2>, 2> matrix) {
+bool cycle_detection(int vertices, int **mst_test) {
+    int flag[vertices];
+    int matrix[vertices][vertices];
+    std::vector<int> cycle_maker;
+
+    for(int i = 0; i < vertices; i++) {
+        flag[i] = 0;
+    }
+
     for(int i = 0; i < vertices; i++) {
         for(int j = 0; j < vertices; j++) {
-            std::cout << matrix[i][j] << " ";
+            matrix[i][j] = mst_test[i][j];
         }
-        std::cout << std::endl;
     }
+
+    bool cycle_status = false;
+    for(int i = 0; i < vertices; i++) {
+        if(flag[i] == 0) flag[i] = 1;
+
+        for(int x = 0; x < cycle_maker.size(); x++) {
+            if(i == cycle_maker[x]) cycle_status = true;
+        }
+
+        if((flag[i] == 1) and (!cycle_status)) {
+            for(int j = 0; j < vertices; j++) {
+                if((matrix[i][j] != 0) and (flag[j] == 0)) {
+                    matrix[i][j] = matrix[j][i] = 0;
+                    flag[j] = 1;
+                } else if((matrix[i][j] != 0) and (flag[j] == 1)) {
+                    cycle_maker.push_back(j);
+                }
+            }
+        }
+    }
+
+    if(cycle_maker.empty()) return false;
+    else return true;
+}
+
+bool connected_graph(int vertices, int **matrix) {
+    int helper_matrix[vertices];
+    
+    for(int i = 0; i < vertices; i++) {
+        helper_matrix[i] = matrix[0][i];
+    }
+
+    for(int i = 0; i < vertices; i++) {
+        if(helper_matrix[i] != 0) {
+            for(int j = 0; j < vertices; j++) {
+                if((matrix[i][j] > 0) and (helper_matrix[j] == 0)) {
+                    helper_matrix[j] = 1;
+                }
+            }
+        }
+    }
+
+    for(int i = 0; i < vertices; i++) {
+        if(helper_matrix[i] == 0) return false;
+    }
+    
+    return true;
 }
--- minimum_spanning_tree/kruskal_algorithm.cpp
+++ minimum_spanning_tree/kruskal_algorithm.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <array>
 #include <math.h>
 #include "graph.h"
 
@@ -11,18 +10,22 @@ int main() {
     ifstream matrix_file;
     int vertices = 0;
     vector<vector<int>> sorted_edges;
-
+    int mst_weight = 0;
     int buffer;
+    
     matrix_file.open("graph.txt");
     while(!matrix_file.eof()) {
         matrix_file >> buffer;
         vertices++;
     }
     matrix_file.close();
-
     vertices = sqrt(vertices);
-    int matrix[vertices][vertices];
-    int mst[vertices][vertices];
+
+    int **matrix;
+    matrix = (int**)malloc(vertices * sizeof(*matrix));
+    for(int i = 0; i < vertices; i++) {
+        matrix[i] = (int*)malloc(vertices * sizeof(int));
+    }
 
     matrix_file.open("graph.txt");
     for(int i = 0; i < vertices; i++) {
@@ -32,6 +35,11 @@ int main() {
     }
     matrix_file.close();
 
+    if(!connected_graph(vertices, matrix)) {
+        cout << "Graph tidak terhubung, proses tidak dapat dilanjutkan." << endl;
+        exit(0);
+    }
+
     for(int i = 0; i < vertices; i++) {
         for(int j = i; j < vertices; j++) {
             if(matrix[i][j] != 0) {
@@ -40,42 +48,66 @@ int main() {
         }
     }
 
-    for(int i = 0; i < vertices; i++) {
+    vector<int> temp;
+    for(int i = 0; i < sorted_edges.size(); i++) {
         int min_idx = i;
-        for(int j = i + 1; j < vertices; j++) {
+        for(int j = i + 1; j < sorted_edges.size(); j++) {
             if(sorted_edges[min_idx][2] > sorted_edges[j][2]) {
                 min_idx = j;
             }
         }
-        vector<int> temp = sorted_edges[i];
+        temp = sorted_edges[i];
         sorted_edges[i] = sorted_edges[min_idx];
         sorted_edges[min_idx] = temp;
     }
 
+
+    int **mst;
+    mst = (int**)malloc(vertices * sizeof(*mst));
+    for(int i = 0; i < vertices; i++) {
+        mst[i] = (int*)malloc(vertices * sizeof(int));
+    }
     for(int i = 0; i < vertices; i++) {
         for(int j = 0; j < vertices; j++) {
             mst[i][j] = 0;
         }
     }
 
+    cout << "Matriks ketetanggan awal:" << endl;
     for(int i = 0; i < vertices; i++) {
+        for(int j = 0; j < vertices; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    cout << endl << "Kruskal's MST Edges:" << endl;
+    for(int i = 0; i < sorted_edges.size(); i++) {
         mst[sorted_edges[i][0]][sorted_edges[i][1]]
             = mst[sorted_edges[i][1]][sorted_edges[i][0]]
             = sorted_edges[i][2];
 
-        
+        if(cycle_detection(vertices, mst)) {
+            mst[sorted_edges[i][0]][sorted_edges[i][1]]
+                = mst[sorted_edges[i][1]][sorted_edges[i][0]]
+                = 0;
+            continue;
+        }
+        mst_weight += sorted_edges[i][2];
+        cout << sorted_edges[i][0] << "-" << sorted_edges[i][1] << " -> ";
+        cout << sorted_edges[i][2] << endl;
     }
 
-    for(int i = 0; i < sorted_edges.size(); i++) {
-        for(int j = 0; j < 3; j++) {
-            cout << sorted_edges[i][j] << " ";
+    
+    cout << endl << "Kruskal's MST:" << endl;
+    for(int i = 0; i < vertices; i++) {
+        for(int j = 0; j < vertices; j++) {
+            cout << mst[i][j] << " ";
         }
         cout << endl;
     }
 
-    // cycle_detection(vertices, matrix);
-    array<array<int, 2>> = {{1, 2}, {3, 4}};
-    coout
+    cout << endl << "Kruskal's MST Weight: " << mst_weight << endl;
 
     return 0;
 }
